46a48c647ac79fb80f5a1e28e6d1ea34995f45aa
Dimitrios Kallimanis <42889879+Dimitrios79@users.noreply.github.com>
2022-02-17 08:53:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from question_model import Question
+from data import question_data
+
+for i in question_data:
+  i = Question()
+  
+question_bank = [i
+
+  
+]
---
+++ question_model.py
@@ -0,0 +1,9 @@
+class Question:
+  
+    def __init__(self, text, answer):
+      self.text = text
+      self.answer = answer
+
+quest_1 = Question("jdksjdks", "False")
+
+print(quest_1.text)
